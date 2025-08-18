66f67c713f2ce7827b8ad8ccfe66b2c296704bde
Pavel Milkevich <pmi@enonic.com>
2025-08-18 16:26:31 +0200

    Go through tutorial and update docs if needed #717

--- code-samples/code-snippets/Rich-text/dataFetcher.ts
+++ code-samples/code-snippets/Rich-text/dataFetcher.ts
@@ -5,9 +5,8 @@ import {helloProcessor} from './components/hello/HelloProcessor';
 import {factboxProcessor} from "./components/macro/FactboxProcessor";
 
 export const dataFetcher = new DataFetcher();
+
 dataFetcher.addContentType('portal:site', {processor: helloProcessor});
 dataFetcher.addCommon({processor: commonProcessor});
 dataFetcher.addContentType('com.enonic.app.hmdb:person', {processor: personProcessor});
-
-// @ts-ignore update type in addMacro
 dataFetcher.addMacro('factbox', {processor: factboxProcessor});
